597f16984977b253ece1edc3fdddfdbb0821543c
Andreagalera <1d09galeraandrea@gmail.com>
2020-03-28 12:07:46 +0100

    Comunication between A-B and A-TTP

--- controllers/clientCtrl.js
+++ controllers/clientCtrl.js
@@ -3,6 +3,7 @@ const controllerCtrl = {};
 
 const rsa = require('rsa');
 const bc = require('bigint-conversion');
+const sha = require('object-sha');
 
 let keyPair;
 
@@ -13,28 +14,44 @@ controllerCtrl.getData = async (req, res) => {
       e: bc.bigintToHex(keyPair["publicKey"]["e"]),
       n: bc.bigintToHex(keyPair["publicKey"]["n"])
     })
-  }catch(err) {
-    res.status(500).send ({ message: err})
+  } catch (err) {
+    res.status(500).send({ message: err })
   }
 }
 
-controllerCtrl.postData =  async (req, res) => {
+controllerCtrl.postData = async (req, res) => {
   try {
     const c = req.body.msg;
     const m = await keyPair["privateKey"].decrypt(bc.hexToBigint(c));
-    res.status(200).send({msg: bc.bigintToHex(m)})
-  }catch(err) {
-    res.status(500).send ({ message: err})
+    res.status(200).send({ msg: bc.bigintToHex(m) })
+  } catch (err) {
+    res.status(500).send({ message: err })
   }
 }
 
-controllerCtrl.signMessage =  async (req, res) => {
+controllerCtrl.signMessage = async (req, res) => {
+  console.log("Sign");
+  console.log(req.body);
   try {
-    const m = bc.hexToBigint(req.body.msg);
+    const m = bc.hexToBigint(req.body.body.msg);
     const s = await keyPair["privateKey"].sign(m);
-    res.status(200).send({msg: bc.bigintToHex(s)})
-  }catch(err) {
-    res.status(500).send ({ message: err})
+    var ts = new Date();
+    const body = {
+      type: "2",
+      src: "B",
+      dest: "A",
+      msg: bc.bigintToHex(s),
+      timestamp: ts
+    };
+    const digest = await sha.digest(body, 'SHA-256');
+    const digestHex = bc.hexToBigint(digest);
+    const signature = await keyPair["privateKey"].sign(digestHex);
+    res.status(200).send({
+      body: body,
+      signature: bc.bigintToHex(signature)
+    })
+  } catch (err) {
+    res.status(500).send({ message: err })
   }
 }
 
