22e540c501f04fedb448bfae9e2a0e4b76f65675
Andreagalera <1d09galeraandrea@gmail.com>
2020-04-04 13:41:29 +0200

    Verify proofs

--- controllers/clientCtrl.js
+++ controllers/clientCtrl.js
@@ -9,11 +9,14 @@ const Crypto = require("crypto");
 
 
 
+
 let keyPair;
 
 let Po;
 
-let  k;
+let k;
+
+let aPubKey;
 
 let cryptotext
 controllerCtrl.getData = async (req, res) => {
@@ -44,7 +47,7 @@ controllerCtrl.signMessage = async (req, res) => {
   try {
     const m = bc.hexToBigint(req.body.msg);
     const s = await keyPair["privateKey"].sign(m);
-    res.status(200).send({msg: bc.bigintToHex(s)})
+    res.status(200).send({ msg: bc.bigintToHex(s) })
   } catch (err) {
     res.status(500).send({ message: err })
   }
@@ -54,35 +57,47 @@ controllerCtrl.noRepudioMessage = async (req, res) => {
   console.log("No repudation");
   console.log(req.body);
   cryptotext = req.body.body.msg;
-  try {
-    const m = bc.hexToBigint(req.body.body.msg);
-    var ts = new Date();
-    const body = {
-      type: "2",
-      src: "B",
-      dest: "A",
-      msg: bc.bigintToText(m),
-      timestamp: ts
-    };
-    const digest = await sha.digest(body, 'SHA-256');
-    const digestHex = bc.hexToBigint(digest);
-    const signature = await keyPair["privateKey"].sign(digestHex);
-    Po = req.signature;
-    res.status(200).send({
-      body: body,
-      signature: bc.bigintToHex(signature)
-    })
-  } catch (err) {
-    res.status(500).send({ message: err })
+  Po = req.body.signature;
+  aPubKey = new rsa.PublicKey(bc.hexToBigint(req.body.pubKey.e), bc.hexToBigint(req.body.pubKey.n));
+  let proofDigest = bc.bigintToHex(await aPubKey.verify(bc.hexToBigint(req.body.signature)));
+  let bodyDigest = await sha.digest(req.body.body);
+  // Comprovar timestamp
+  console.log("Timestamp");
+  var tsB = new Date();
+  var tsA = req.body.body.timestamp;
+  tsA = new Date(tsA);
+  var seconds = (tsB.getTime() - tsA.getTime()) / 1000;
+  console.log(seconds);
+  if ((bodyDigest === proofDigest) && (seconds < 1)) {
+    try {
+      const m = bc.hexToBigint(req.body.body.msg);
+      const body = {
+        type: "2",
+        src: "A",
+        dest: "B",
+        msg: bc.bigintToText(m),
+        timestamp: tsB
+      };
+      const digest = await sha.digest(body, 'SHA-256');
+      const digestHex = bc.hexToBigint(digest);
+      const signature = await keyPair["privateKey"].sign(digestHex);
+      res.status(200).send({
+        body: body,
+        signature: bc.bigintToHex(signature)
+      })
+    } catch (err) {
+      res.status(500).send({ message: err })
+    }
   }
+  else { console.log("Pruebas malamente"); }
 }
 
-function getK(){
+function getK() {
   console.log("getK");
-require('request')('http://localhost:3001/api/clientes/downloadK', (err, res, body) => {
-  console.log(body);
-  k = body;
-  decrypt(k);
+  require('request')('http://localhost:3001/api/clientes/downloadK', (err, res, body) => {
+    console.log(body);
+    k = body;
+    decrypt(k);
   });
 }
 
